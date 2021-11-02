3ccfaa04b7c14d755f1a91d52115802114d86610
Prudhvi Karanam <prudhvikaranam537@gmail.com>
2021-11-02 15:24:40 +0530

    Adding toggling functionality  to show and hide the data

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <head>
-    <style> 
-
-    </style>
+    <link rel="stylesheet" href="accordian.css" />
 </head>
-
-
 <div class="ptk-accordian-container" id="ptk-accordian-container">
 
 </div>`;
@@ -21,20 +17,44 @@ class ptfAccordian extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
   connectedCallback() {
+    let a = 0;
     convertedData.forEach((data, i) => {
+      a = i;
       const accordian = document.createElement("div");
       accordian.classList = `accordian accordian-${i}`;
       accordian.id = `accordian`;
-      console.log(accordian);
       accordian.innerHTML = `
-            <h1>${data.headerName}</h1>
-            <p>${data.data}</p>
+            <div class="accordian-head" id="accordian-head">
+              <span><h1 class="header">${data.headerName}</h1></span>
+              <span class="toggleData" id="toggleData">Show</span>
+            </div>
+            <div class="accordian-data" id="accordian-data">
+              <p class="data" id="data">${data.data}</p>
+            </div>
         `;
+
       this.shadowRoot
         .getElementById("ptk-accordian-container")
         .appendChild(accordian);
+    });
+    this.shadowRoot.querySelectorAll("#toggleData").forEach((data, i) => {
+      data.addEventListener("click", () => {
+        const getDataClass =
+          this.shadowRoot.querySelectorAll("#accordian-data")[i].classList;
+        let getButtonContent =
+          this.shadowRoot.querySelectorAll("#toggleData")[i];
+
+        if (getDataClass.contains("show")) {
+          getDataClass.remove("show");
+          getButtonContent.innerHTML = "Show";
+        } else {
+          getDataClass.add("show");
+          getButtonContent.innerHTML = "Hide";
+        }
+      });
     });
   }
 }
 
 window.customElements.define("ptk-accordian", ptfAccordian);
+
